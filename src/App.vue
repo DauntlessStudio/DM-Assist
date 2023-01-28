22a5360096c7a0aac31548aad2695f935145f2e0
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-01-27 18:55:17 -0600

    Updated Template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="root">
+    <div class="nav">nav</div>
+    <div class="main">main</div>
+    <div class="notes">notes</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -21,6 +20,38 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+html, body {
+  margin: 0;
+  padding: 0;
+}
+body {
+  height: 100vh;
+  width: 100vw;
+}
+.root {
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-areas:
+    'nav main main main main'
+    'nav main main main main'
+    'nav notes notes notes notes';
+}
+.nav {
+  background-color: blue;
+  grid-area: nav;
+}
+.main {
+  background-color: orange;
+  grid-area: main;
+}
+.notes {
+  background-color: lavender;
+  grid-area: notes;
 }
 </style>
