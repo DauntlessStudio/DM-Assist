7c2029b2a512fe4f713c158872739aeaa521a67a
DauntlessStudio <caleb@cookingblock.com>
2024-09-16 09:08:48 -0700

    General Layout

---
+++ src/lib/components/DiceBox.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let list: string[] = [
+        "/images/d4.png",
+        "/images/d6.png",
+        "/images/d8.png",
+        "/images/d10.png",
+        "/images/d12.png",
+        "/images/d20.png",
+    ];
+
+    let gap = 2;
+</script>
+
+<style>
+    .container {
+        display:grid;
+        grid-template-columns:repeat(auto-fit, minmax(100px, 1fr));
+    }
+    .red-filter {
+        filter: brightness(0) saturate(100%) invert(15%) sepia(100%) saturate(6932%) hue-rotate(0deg) brightness(100%) contrast(115%);
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+<div>
+    <div class=container style={'grid-gap:'+gap+'px'}>
+        {#each list as url}
+            <img src={url} alt="D20 die" class="red-filter"/>
+        {/each}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Pane, Splitpanes } from 'svelte-splitpanes';
+    import DiceBox from '$lib/components/DiceBox.svelte';
+</script>
+
+<Splitpanes style="height:100vh">
+	<Pane minSize={20} size={20}>
+		<DiceBox/>
+	</Pane>
+	<Pane>
+        2
+	</Pane>
+	<Pane minSize={25} size={25}>
+        3
+    </Pane>
+</Splitpanes>
