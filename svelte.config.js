566be0fd4ee671882bf1f7f4bd839d830cb3c3ab
DauntlessStudio <caleb@cookingblock.com>
2024-09-23 22:27:47 -0500

    Updated Config

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false
-        }),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/DM-Assist' : '',
-        }
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+
+		paths: {
+			base: '/DM-Assist',
+		},
+
+		// This setting is crucial for SPA (Single Page Application) routing
+		appDir: 'app',
 	}
 };
 
